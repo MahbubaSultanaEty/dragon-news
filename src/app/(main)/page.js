800908efa15bc1b4022107504822b83bce21d5f4
Mahbuba-S-Ety <sultanamahbuba09@gmail.com>
2026-04-29 23:53:21 +0600

    added all the catagories from catagories api dynmically

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,7 +1,37 @@
 import Image from "next/image";
 
-export default function Home() {
+async function getCatagories() {
+  const res = await fetch('https://openapi.programming-hero.com/api/news/categories');
+  const data = await res.json();
+  return data.data
+}
+
+export default async function Home() {
+
+  const catagoriesData = await getCatagories();
+  const newsCatagories = catagoriesData.news_category;
+
+  console.log(newsCatagories, "news catagory");
+
   return (
-    <h2 className="text-3xl ">Home Page</h2>
+    <div className="grid grid-cols-12 gap-4">
+      <div className=" font-bold text-2xl col-span-3">
+        All Catagories
+        <ul className="flex flex-col gap-2 mt-5">
+           {
+          newsCatagories.map((catagory) => {
+            return <li className="bg-slate-400 rounded-md font-bold text-center text-lg" key={catagory.category_id}>{ catagory.category_name}</li>
+          })
+        }
+      </ul>
+       
+      </div>
+      <div className="bg-red-100 font-bold text-2xl col-span-6">
+        All News
+      </div>
+      <div className="bg-blue-100 font-bold text-2xl col-span-3">
+        Social Catagories
+      </div>
+    </div>
   );
 }
